76b9049c9c682b5d95ec467dc018129481e2d8b4
jiangtao5566 <834821058@qq.com>
2022-04-04 01:00:11 +0800

    项目结构目录设置

---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavFooter from '../components/NavFooter.vue'
+import NavHeader from '../components/NavHeader.vue'
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'home',
+  components: { 
+    NavFooter,
+    NavHeader
+  }
+  
+}
+</script>
